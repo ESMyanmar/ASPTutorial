57f33573530e1f21bdaa5d6d86e71d53797f3bf1
Kyaw Myat Moe <developer.kyawmyatmoe@gmail.com>
2020-06-29 16:13:36 +0900

    Add files via upload
    First try.

---
+++ ASPStepByStep/ASPStepByStep/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" Inherits="ASPStepByStep.Default" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>Default</title>
+</head>
+<body>
+	
+    <h1>Hello World!</h1>
+    <%
+        ShowLineage();
+    %>
+</body>
+</html>
---
+++ ASPStepByStep/ASPStepByStep/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace ASPStepByStep
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        /*public void button1Clicked(object sender, EventArgs args)
+        {
+            button1.Text = "You clicked" + Request.Form["Feature"];
+        }*/
+        public void ShowLineage()
+        {
+            Response.Write("Check out the family tree: <br/> <br/>");
+            Response.Write(this.GetType().ToString());
+            Response.Write(" which derives from: <br/> ");
+            Response.Write(this.GetType().BaseType.ToString());
+            Response.Write(" which derives from: <br/> ");
+            Response.Write(this.GetType().BaseType.BaseType.ToString());
+            Response.Write(" which derives from: <br/> ");
+            Response.Write(this.GetType().BaseType.BaseType.BaseType.ToString());
+            Response.Write(" which derives from: <br/> ");
+            Response.Write(this.GetType().BaseType.BaseType.BaseType.BaseType.ToString());
+        }
+    }
+}
